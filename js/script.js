65d4d93414d0539b320a988797856c85d28c9f57
MCillo <117599499+MCillo@users.noreply.github.com>
2023-04-15 20:25:12 -0400

    Update script.js
    added the "Vanilla" javascript provided by CF to be converted into JQuery

--- js/script.js
+++ js/script.js
@@ -1,1 +1,41 @@
 // To-Do list App 
+// Vanilla Javascript was provided by "schoolhouse"
+function newItem() {
+
+    // javascript
+    // 1. Adding a New Item
+    let li = document.createElement('li');
+    let inputValue = document.getElementById('input').ariaValueMax;
+    let text = document.createTextNode(inputValue);
+    li.appendChild(text);
+
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        let list = document.querySelector('#list');
+        list.appendChild(li);
+    }
+
+    // 2. Crossing out an item from the list
+    function crossOut() {
+        li.classList.toggle('strike');
+    }
+
+    li.addEventListener('dblclick', crossOut);
+
+    // 3a. Adding the delete button "X"
+    let crossOutButton = document.createElement('crossOutButton');
+    crossOutButton.appendChild(document.createTextNode('X'));
+    li.appendChild(crossOutButton);
+
+    crossOutButton.addEventListener('click', deleteListItem);
+
+    // 3b. Adding Class Delete (display:none) from the css
+    function deleteListItem() {
+        li.classList.add('delete')
+    }
+
+    // 4. Reordering the items
+    $('#list').sortable();
+}
+
